11415f3b1e4f650aa795bdae8ecc5b29913575ca
Alejandro Garcia <a.garcia.alfonso@gmail.com>
2020-10-12 01:32:25 +0200

    Update check_pull_request.groovy

--- vars/check_pull_request.groovy
+++ vars/check_pull_request.groovy
@@ -1,1 +1,28 @@
-def check_pull_request()
+import org.com.utils.checkdUtils
+
+def call() {
+  def check_Utils = new checkUtils()
+  
+  pipeline {
+    agent {
+      label 'agent_maven'
+    }
+    stages {
+      stage('validate security code') {
+        steps {
+          script {
+            image = build_utils.get_image()
+            version = build_utils.get_version()
+            result_vaidation = check_Utils.security()
+          }
+        }
+      }
+    }
+    post
+  
+  
+  
+  
+  
+  }
+}
